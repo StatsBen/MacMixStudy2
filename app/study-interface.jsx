aed77686c83dc8df76d1b115892dd652a6446fa2
Ben <ben.clark456@gmail.com>
2016-10-25 16:19:24 -0700

    still adding initial files... hold on

---
+++ app/study-interface.jsx
@@ -0,0 +1,27 @@
+/**
+ *  The outermost component that'll render the study
+ *   interface for the second part of the Macaron Mix
+ *    study which investigates algorithms for mixing
+ *     haptic icons in a "perceptually soud" way.
+ *
+ *  We'll be using a technique from the world of
+ *   psychophysics called the "staircase" method
+ *
+ *  Author: Ben Clark - Oct. 2016
+ **/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var StudyInterface = React.createClass({
+
+  render: function() {
+    return(
+      <div id="app">
+        <h1>T E S T</h1>
+      </div>
+    );
+  }
+
+});
+
+module.exports = StudyInterface;
